6a55796e561bcbc9fb4d089cc401b6d0f0e1760c
dorudorujr <a1209s518n928@gmail.com>
2018-04-22 04:49:46 +0900

    fix

--- Connect/AppDelegate.swift
+++ Connect/AppDelegate.swift
@@ -7,17 +7,17 @@
 //
 
 import UIKit
-import Firebase
+//import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
   var window: UIWindow?
   
-  override init() {
-    super.init()
-    FirebaseApp.configure()
-  }
+//  override init() {
+//    super.init()
+////    FirebaseApp.configure()
+//  }
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
